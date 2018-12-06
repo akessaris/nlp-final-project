3043e186b269ff75ce46bc852749a3f643d5602e
Josh Mendelsohn <joshmendelsohn2@gmail.com>
2018-12-06 04:49:36 +0000

    score.py now calculates f-measure

--- score.py
+++ score.py
@@ -18,6 +18,7 @@ def main():
     answerFile = open(args.answerFileName,'r')
 
     totalPrecision = 0
+    totalRecall = 0
     totalLines = 0
     for genLine, answerLine in izip(genFile,answerFile):
         if genLine == None or answerLine == None:
@@ -28,19 +29,36 @@ def main():
         for code in answerCodes:
             codeDict[code] = True
 
+        genCodeDict = {}
         correctCodes = 0
         for code in genCodes:
             if code in codeDict:
-                correctCodes+=1
+                correctCodes += 1
+            genCodeDict[code] = True
+
+        recalledCodes = 0
+        for code in answerCodes:
+            if code in genCodeDict:
+                recalledCodes += 1
+
 
         precision = correctCodes / len(genCodes)
+        recall = recalledCodes / len(answerCodes)
+
+
         # print precision
         totalPrecision += precision
-        totalLines+=1
+        totalRecall += recall
+        totalLines += 1
 
-    totalPrecision/=totalLines
+    averagePrecision = totalPrecision/totalLines
+    averageRecall = totalRecall/totalLines
 
-    print "Precision: " + str(totalPrecision)
+    fmeasure = 2 / ((1 / averagePrecision) + (1 / averageRecall))
+
+    print "Average precision: " + str(averagePrecision)
+    print "Average recall: " + str(averageRecall)
+    print "F-measure of the averages: " + str(fmeasure)
 
 
 if __name__ == '__main__':
